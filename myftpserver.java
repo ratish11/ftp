c5e5091921b18adea114d3db6be38cb595b9a096
Ratish Jha <56468086+ratish11@users.noreply.github.com>
2023-03-15 15:32:54 -0400

    Update myftpserver.java

--- myftpserver.java
+++ myftpserver.java
@@ -1,29 +1,81 @@
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 public class myftpserver {
-    public static void main(String args[]) {
-        if(args.length != 1){System.out.println("Error: invalid arguments");System.exit(0);}
-        if(Integer.parseInt(args[0]) <= 1023 || Integer.parseInt(args[0]) > 65535) {
+    private static HashMap<String, Boolean> processRecord; //processRecord(cid, command finish status) True=command terminated
+    private static HashMap<String, Boolean> lockRecord; //lockRecord(path, lock status) true=file is locked
+    private static int nport = 0;
+    private static int tport = 0;
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Error: invalid arguments");
+            System.exit(0);
+        }
+        try {
+            nport = Integer.parseInt(args[0]);
+            tport = Integer.parseInt(args[1]);
+        } catch (NumberFormatException portEx){
+            System.out.println("Error: invalid port type provided, port must be an integer value : " + portEx);
+            System.exit(0);
+        } catch (Exception e) {
+            // Handle exception
+            e.printStackTrace();
+            System.out.println("Error - Connection Failed");
+            System.exit(0);
+        }
+        if (nport <= 1023 || nport > 65535) {
             System.out.println("Error: invalid port range, port range 1024 to 65535 are valid for non system processes");
             System.exit(0);
         }
-//      try inititlizing a server
+        if (tport <= 1023 || tport > 65535) {
+            System.out.println("Error: invalid port range, port range 1024 to 65535 are valid for non system processes");
+            System.exit(1);
+        }
+
+        processRecord = new HashMap<>();
+        lockRecord = new HashMap<>();
+//        Thread cleaning = new Thread(new GarbageCollector(processRecord, lockRecord));
+//        cleaning.start();
+        Thread commandProcess = new Thread(new CommandServer(nport, processRecord, lockRecord));
+        commandProcess.start();
+        Thread terminateProcess = new Thread(new TerminateServer(tport, processRecord));
+        terminateProcess.start();
+    }
+}
+
+class CommandServer implements Runnable{
+    // private ServerSocket sS;
+    //private Socket s;
+    private int port;
+    HashMap<String, Boolean> processRecord;
+    HashMap<String, Boolean> lockRecord;
+    public CommandServer(int port, HashMap<String, Boolean> processRecord, HashMap<String, Boolean> lockRecord){
+        this.port = port;
+        this.processRecord = processRecord;
+        this.lockRecord = lockRecord;
+    }
+
+    @Override
+    public void run() { //non synchronized run
         try {
 //            binding server at the provided port
-            ServerSocket sS = new ServerSocket(Integer.parseInt(args[0]));
-            System.out.println("Server started on port " + args[0]);
+            ServerSocket sS = new ServerSocket(port);
+            System.out.println("Command Server started on port " + port);
             while(true) {
-//            accept clients incomming connection
+//            accept clients incoming connection
                 try {
 //                establish the connection with a new client
                     Socket client = sS.accept();
                     System.out.println("New client connected : " + client.getInetAddress().getHostAddress());
 //                    creating threads to talk to multiple clients
-                    new Thread(new ClientThreadHandler(client)).start();
+                    new Thread(new ClientThreadHandler(client, processRecord, lockRecord)).start();
                 }
                 catch (IOException e) {
                     e.printStackTrace();
@@ -36,26 +88,126 @@ public class myftpserver {
         catch (Exception e) {System.out.println("Error: following error caught: " + e);}
     }
 }
-class ClientThreadHandler implements Runnable {
+class TerminateServer implements Runnable{
+    //private ServerSocket sS;
+    //private Socket s;
+    private int port;
+    HashMap<String, Boolean> processRecord;
+
+    public TerminateServer(int port, HashMap<String, Boolean> processRecord){
+        this.port = port;
+        this.processRecord = processRecord;
+    }
+
+    @Override
+    public void run() { //non synchronized run
+        try {
+//            binding server at the provided port
+            ServerSocket sS = new ServerSocket(port);
+            System.out.println("Terminate Server started on port " + port);
+            while(true) {
+//            accept clients incoming connection
+                try {
+//                establish the connection with client
+                    Socket client = sS.accept();
+                    System.out.println("New client connected on tport : " + client.getInetAddress().getHostAddress());
+//                    creating threads to terminate service
+                    new Thread(new TerminateService(client,processRecord)).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("Error: unable to talk to the client with the following error : " + e);
+                }
+            }
+        }
+        catch (NumberFormatException portEx) {System.out.println("Error: invalid port type provided, port must be an integer value : " + portEx);}
+        catch (IOException io) {io.printStackTrace();System.out.println("Error: unable to initialize the server : " + io);System.exit(0);}
+        catch (Exception e) {System.out.println("Error: following error caught: " + e);}
+    }
+}
+
+class TerminateService implements Runnable{
+    private Socket s;
+    private DataOutputStream dos;
+    private DataInputStream dis;
+    HashMap<String, Boolean> processRecord;
+
+    public TerminateService(Socket s, HashMap<String, Boolean> processRecord)
+    {
+        // Instantiate data members
+        try {
+            this.s = s;
+            dos = new DataOutputStream(s.getOutputStream());
+            dis = new DataInputStream(s.getInputStream());
+            this.processRecord = processRecord;
+        } catch (IOException ex) {
+            Logger.getLogger(TerminateService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void run() { //non synchronized run
+        try {
+            String command = dis.readUTF();
+            System.out.println(command);
+            terminate(command);
+            s.close();
+        } catch (IOException ex) {
+            Logger.getLogger(TerminateService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public synchronized void terminate(String command) {
+        if(command.length() != 14 || !command.contains(" ")) {
+            try {
+                dos.writeUTF("Error: Invalid argument");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        String cid = command.split(" ")[1];
+        if(processRecord.containsKey(String.valueOf(cid))) {
+            try {
+                processRecord.put(cid, Boolean.TRUE);
+                dos.writeUTF("TERMINATE "+cid);
+            } catch (IOException ex) {
+                Logger.getLogger(TerminateService.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        else {
+            try {
+                dos.writeUTF("Error: Command ID not found");
+            } catch (IOException ex) {
+                Logger.getLogger(TerminateService.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
+
+
+class ClientThreadHandler implements Runnable{
     private Socket clientInstance;
     private DataOutputStream dos;
     private DataInputStream dis;
-//    ClientThreadHandler Constructor
-    public ClientThreadHandler(Socket client) {
+    HashMap<String, Boolean> processRecord;
+    HashMap<String, Boolean> lockRecord;
+
+    public ClientThreadHandler(Socket client, HashMap<String, Boolean> processRecord, HashMap<String, Boolean> lockRecord) {
         try {
             this.clientInstance = client;
-//            get data from client
-            dis = new DataInputStream(clientInstance.getInputStream());
-//            send data to client
-            dos = new DataOutputStream(clientInstance.getOutputStream());
+            this.processRecord = processRecord;
+            this.lockRecord = lockRecord;
+//            dis = new DataInputStream(clientInstance.getInputStream());
+//            dos = new DataOutputStream(clientInstance.getOutputStream());
         }
-        catch (IOException io) {
-            Logger.getLogger(ClientThreadHandler.class.getName()).log(Level.SEVERE, null, io);
+        catch(Exception e){
+            Logger.getLogger(ClientThreadHandler.class.getName()).log(Level.SEVERE, null, e);
         }
     }
     public void run() {
-        Logger.getLogger(ClientThreadHandler.class.getName()).log(Level.INFO, "Cilent Connected");
+        Logger.getLogger(ClientThreadHandler.class.getName()).log(Level.INFO, "Client Connected");
         try {
+            dis = new DataInputStream(clientInstance.getInputStream());
 //                listening continuously to what client has to say
             while(true) {
                 String cmd = dis.readUTF();
@@ -76,7 +228,6 @@ class ClientThreadHandler implements Runnable {
                 if(cmd.split(" ", 2)[0].equals("ls"))
                     list(cmd);
                 if(cmd.trim().equals("quit")) {
-                    System.out.println("Client " + clientInstance.getInetAddress().getHostAddress() + " leaving.....");
                     clientInstance.close();
                     break;
                 }
@@ -85,9 +236,12 @@ class ClientThreadHandler implements Runnable {
             Logger.getLogger(ClientThreadHandler.class.getName()).log(Level.SEVERE, null, io);
         }
     }
-    public void get(String cmd, String address) {
+
+    public synchronized void get(String cmd, String address) {
 //        System.out.println(cmd.split(" ").length);
-        if(!cmd.contains(" ") || cmd.split(" ").length < 2) {
+        try {dos = new DataOutputStream(clientInstance.getOutputStream());}
+        catch(IOException io) {io.printStackTrace();}
+        if (!cmd.contains(" ") || cmd.split(" ").length < 2) {
             try {
                 System.out.println("Invalid get command recevied");
                 dos.writeUTF("Error: Invalid 'get' command, check man page for details");
@@ -96,39 +250,69 @@ class ClientThreadHandler implements Runnable {
             }
         }
 //        get the filename to be transfered
-        String file = cmd.split(" ")[cmd.split(" ").length-1];
-        File cdir=new File(System.getProperty("user.dir"));
+        String file = cmd.split(" ")[1];
+        File cdir = new File(System.getProperty("user.dir"));
         File sendFile = new File(cdir, file);
         try {
-            if(!sendFile.exists() || !sendFile.canRead()) {
+            if (!sendFile.exists() || !sendFile.canRead()) {
 //                error if file doesn't exists or is unreadable by access
                 dos.writeUTF("Error: cannot access '" + file + "': No such file or directory or unable to read file");
                 return;
-            }
-            else {
+            } else {
                 dos.writeUTF("receiving " + sendFile.length() + " bytes...");
-                System.out.println("sending sendFile " + file + " to client " + address);
+                System.out.println("sending File " + file + " to client " + address);
+            }
+            String id;
+            while (true) {
+                UUID processID = UUID.randomUUID();
+                id = processID.toString().substring(0, 4);
+                if (processRecord.containsKey(id))
+                    continue;
+                else {
+                    processRecord.put(id, Boolean.FALSE);
+                    dos.writeUTF(id.toString());
+                    break;
+                }
+            }
+            String path = sendFile.getAbsolutePath();
+            while (true) {
+                if (!lockRecord.containsKey(path)) {
+                    lockRecord.put(path, true);
+                    break;
+                } else {
+                    Thread.sleep(500);
+                }
             }
 //            open file that is to be transfered
             FileInputStream fis = new FileInputStream(sendFile);
             int bytes = 0;
             dos.writeLong(sendFile.length());
 //            break file in chucks and send it to client
-            byte[] buffer = new byte[4*1024];
-            while((bytes = fis.read(buffer)) != -1) {
+            byte[] buffer = new byte[4 * 1024];
+            while ((bytes = fis.read(buffer)) != -1) {
+                if (processRecord.get(id).equals(Boolean.TRUE)) {
+                    fis.close();
+                    lockRecord.remove(id);
+                    processRecord.remove(id);
+                    return;
+                }
                 dos.write(buffer, 0, bytes);
-                dos.flush();
             }
+            lockRecord.remove(path);
+            processRecord.remove(id);
             System.out.println("file transferred");
-//            dos.writeUTF("file received..");
+            dos.writeUTF("file received..");
 //            close the file after transfer
             fis.close();
             dos.flush();
         } catch (IOException io) {
             Logger.getLogger(ClientThreadHandler.class.getName()).log(Level.SEVERE, null, io);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ClientThreadHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public void put(String cmd, String address) {
+
+    public synchronized void put(String cmd, String address) {
         if(!cmd.contains(" ") || cmd.split(" ").length < 2) {
             try {
                 System.out.println("Invalid put command recevied");
@@ -167,8 +351,11 @@ class ClientThreadHandler implements Runnable {
             dos.flush();
         } catch (IOException ex) {
             Logger.getLogger(ClientThreadHandler.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        } /*catch (InterruptedException ex) {
+            Logger.getLogger(ClientThreadHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
     }
+
     public void chdir(String cmd, String address) {
         if(!cmd.contains(" ") || cmd.split(" ").length < 2) {
             try {
@@ -207,8 +394,11 @@ class ClientThreadHandler implements Runnable {
         }
     }
     public void abspath(String cmd) {
+        try {dos = new DataOutputStream(clientInstance.getOutputStream());}
+        catch(IOException io) {io.printStackTrace();}
         File cdir = new File(System.getProperty("user.dir"));
         try {
+            System.out.println("Sending present working directory : " + String.valueOf(cdir));
             dos.writeUTF(String.valueOf(cdir));
             dos.flush();
         } catch (IOException io) {
@@ -216,6 +406,8 @@ class ClientThreadHandler implements Runnable {
         }
     }
     public void delete(String cmd){
+        try {dos = new DataOutputStream(clientInstance.getOutputStream());}
+        catch(IOException io) {io.printStackTrace();}
         if(!cmd.contains(" ") || cmd.split(" ").length < 2) {
             try {
                 System.out.println("Invalid delete command recevied");
@@ -223,6 +415,7 @@ class ClientThreadHandler implements Runnable {
             } catch (IOException io) {
                 io.printStackTrace();
             }
+            return;
         }
 //      File to be deleted
         String file = cmd.split(" ")[1];
@@ -231,7 +424,8 @@ class ClientThreadHandler implements Runnable {
         try {
             if(!deleteFile.exists() || !deleteFile.canWrite()) {
 //                error if file doesn't exists or cannot be modified
-                dos.writeUTF("Error: Invalid filename or file doesn't exist");
+                dos.writeUTF("Error: no such file or directory exist");
+                System.out.println("Error: no such file or directory exist");
                 return;
             }
             if(deleteFile.delete()){
@@ -240,13 +434,14 @@ class ClientThreadHandler implements Runnable {
             }
             else{
                 dos.writeUTF("Error: Unable to delete file "+file);
-                System.out.println("INFO: " + file + " deleted successfully");
             }
         } catch (IOException io) {
             io.printStackTrace();
         }
     }
     public void mkdir(String cmd) {
+        try {dos = new DataOutputStream(clientInstance.getOutputStream());}
+        catch(IOException io) {io.printStackTrace();}
         if(!cmd.contains(" ") || cmd.split(" ").length < 2) {
             try {
                 System.out.println("Invalid mkdir command recevied");
@@ -278,6 +473,8 @@ class ClientThreadHandler implements Runnable {
         }
     }
     public void list(String cmd) {
+        try {dos = new DataOutputStream(clientInstance.getOutputStream());}
+        catch(IOException io) {io.printStackTrace();}
         try{
             File lsPath = null;
             File[] fileList = null;
@@ -285,7 +482,7 @@ class ClientThreadHandler implements Runnable {
             File file=new File(cdir.getAbsolutePath());
 //            System.out.println(String.valueOf(file));
 //            System.out.println(cmd.split(" ").length);
-            if(cmd.equals("ls") || cmd.split(" ")[cmd.split(" ").length -1].equals(".")) {
+            if(cmd.equals("ls") || (cmd.equals("ls") && cmd.split(" ")[cmd.split(" ").length -1].equals("."))) {
                 lsPath = file;
                 System.out.println(String.valueOf(lsPath));
                 dos.writeUTF("sending list...");
@@ -306,18 +503,12 @@ class ClientThreadHandler implements Runnable {
                     return;
                 }
             }
-            if(lsPath.canRead()) {
-//                if read permission
-                dos.writeUTF("sending files..");
-                fileList = lsPath.listFiles();
-//                Create object output stream
-                ObjectOutputStream outFiles = new ObjectOutputStream(clientInstance.getOutputStream());
-//                Write the files to output stream
-                System.out.println("INFO: sending file list..");
-                outFiles.writeObject(fileList);
-            } else {
-                dos.writeUTF("Error: Permission denied");
-            }
+            fileList = lsPath.listFiles();
+//            Create object output stream
+            ObjectOutputStream outFiles = new ObjectOutputStream(clientInstance.getOutputStream());
+//            Write the files to output stream
+            System.out.println("INFO: sending file list..");
+            outFiles.writeObject(fileList);
         } catch (IOException io) {
             io.printStackTrace();
         } catch (Exception e) {
@@ -325,4 +516,5 @@ class ClientThreadHandler implements Runnable {
             System.out.println(e);
         }
     }
+
 }
