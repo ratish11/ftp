af58565d47a45df3a3df907c2b52fed0be2a5a9d
Ratish Jha <56468086+ratish11@users.noreply.github.com>
2023-03-17 00:41:06 -0400

    import java.util.Arrays;

--- client/myftp.java
+++ client/myftp.java
@@ -5,6 +5,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.Arrays;
 
 public class myftp {
     private static Socket s = null;
